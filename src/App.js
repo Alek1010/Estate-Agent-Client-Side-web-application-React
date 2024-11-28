97647c08112a964e52134a9243b641b7949a50c6
Alek1010 <ali.mihaylov04@gmail.com>
2024-11-28 19:04:14 +0000

    Added_Properties_Page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Footer from './Components/Navbar/Footer';
 import Aboutus from './Components/Aboutus/Aboutus';
 import ContactUs from './Components/ContactUs/ContactUs';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Properties from './Components/PropertiesPage/Properties';
+import Property from './Components/PropertiesPage/Property';
+import { FavoriteProvider } from './Components/PropertiesPage/FavoriteContext';
 
 
 
@@ -16,13 +18,17 @@ function App() {
 
   return (
    <BrowserRouter>
+   <FavoriteProvider>
     <NavBar />
     <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/aboutus" element={<Aboutus />} />
     <Route path='/ContactUs' element={<ContactUs />} />
+    <Route path="/properties" element={<Properties />} />
+    <Route path="/properties/:id" element={<Property />} />
     </Routes>
     <Footer/>
+    </FavoriteProvider>
    </BrowserRouter>
   );
 }
